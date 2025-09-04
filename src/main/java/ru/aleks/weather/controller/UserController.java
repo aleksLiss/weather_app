78ab973e82e08672da048893d388472c0477ec70
aleks <darklissik.darklissik@gmail.com>
2025-09-04 22:18:16 +0300

    change UserController.java

--- src/main/java/ru/aleks/weather/controller/UserController.java
+++ src/main/java/ru/aleks/weather/controller/UserController.java
@@ -1,33 +1,42 @@
 package ru.aleks.weather.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.aleks.weather.dto.UserDto;
 import ru.aleks.weather.model.User;
 
 @Controller
-@RequestMapping("/")
 public class UserController {
 
-    @GetMapping("user/in")
-    public String getSignIn() {
-        return "sign/sign-in";
-    }
-
     @GetMapping("/user/up")
-    public String getSignUpPage() {
+    public String getSignUpPage(Model model) {
+        model.addAttribute("userDto", new UserDto());
         return "sign/sign-up";
     }
 
-    @PostMapping
-    public String registerUser(@ModelAttribute UserDto userDto) {
-        System.out.println(userDto.getUsername());
-        System.out.println(userDto.getPassword());
-        System.out.println(userDto.getRepeatPassword());
+    @PostMapping("/user/up")
+    public String registerUser(@ModelAttribute UserDto userDto, Model model) {
+
+        UserDto registeredUser = userDto;
+
+        model.addAttribute("message", userDto.getUsername());
+        return "index";
+    }
+
+    @GetMapping("/user/in")
+    public String getSingInPage(Model model) {
+        model.addAttribute("user", new User());
+        return "sign/sign-in";
+    }
+
+    @PostMapping("/user/in")
+    public String signInUser(@ModelAttribute User user) {
         try {
 
         } catch (Exception ex) {
-            return "errors/error";
+
         }
         return "index";
     }
