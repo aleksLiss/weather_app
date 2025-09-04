5d72bd9737b321150d96188f35d67948a0e94c03
aleks <darklissik.darklissik@gmail.com>
2025-09-04 22:18:04 +0300

    add UserRepository.java

---
+++ src/main/java/ru/aleks/weather/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package ru.aleks.weather.repository;
+
+import ru.aleks.weather.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+
+    boolean save(User user);
+
+    boolean delete(int id);
+
+    Optional<User> getById(int id);
+
+    Optional<User> getByLogin(String login);
+
+}
