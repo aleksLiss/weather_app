8930cc89a63f319a5ee4fa2ffe0f6a375b2ed0ee
aleks <darklissik.darklissik@gmail.com>
2025-09-04 22:03:25 +0300

    add database config

---
+++ src/main/java/ru/aleks/weather/config/DatabaseConfig.java
@@ -0,0 +1,38 @@
+package ru.aleks.weather.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@Component
+@PropertySource("classpath:application.properties")
+public class DatabaseConfig {
+
+
+    @Autowired
+    Environment environment;
+
+    private final String URL = "spring.datasource.url";
+    private final String USERNAME = "spring.datasource.username";
+    private final String PASSWORD = "spring.datasource.password";
+    private final String DRIVER = "spring.datasource.driver-class-name";
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(environment.getProperty(URL));
+        driverManagerDataSource.setUsername(environment.getProperty(USERNAME));
+        driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
+        driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+        return driverManagerDataSource;
+    }
+}
