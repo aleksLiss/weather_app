f651d61173fe705dc8698944a7cddb54822deac2
aleks <darklissik.darklissik@gmail.com>
2025-09-04 22:09:12 +0300

    add UserMapper.java

---
+++ src/main/java/ru/aleks/weather/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package ru.aleks.weather.mapper;
+
+import ru.aleks.weather.model.User;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setLogin(resultSet.getString("login"));
+        user.setPassword(resultSet.getString("password"));
+        return user;
+    }
+}
