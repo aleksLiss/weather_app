258f976201891c089a546862c77d5a820214e5db
aleksLiss <darklissik.darklissik@gmail.com>
2025-09-07 19:01:58 +0300

    create post method for register user

--- src/main/java/ru/aleks/weather/controller/UserController.java
+++ src/main/java/ru/aleks/weather/controller/UserController.java
@@ -1,27 +1,46 @@
 package ru.aleks.weather.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.aleks.weather.dto.UserDto;
+import ru.aleks.weather.dto.SaveUserDto;
 import ru.aleks.weather.model.User;
+import ru.aleks.weather.service.UserService;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
 
+    // todo create integrate test
+    // todo create httpsession logic
+
+    private final UserService userService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/user/up")
     public String getSignUpPage(Model model) {
-        model.addAttribute("userDto", new UserDto());
+        model.addAttribute("userDto", new SaveUserDto());
         return "sign/sign-up";
     }
 
     @PostMapping("/user/up")
-    public String registerUser(@ModelAttribute UserDto userDto, Model model) {
-
-        UserDto registeredUser = userDto;
-
-        model.addAttribute("message", userDto.getUsername());
+    public String registerUser(@ModelAttribute SaveUserDto userDto, Model model, HttpSession session) {
+        Optional<User> savedUser = userService.save(userDto);
+        if (savedUser.isEmpty()) {
+            LOGGER.warn("UserController: error register new user");
+            model.addAttribute("message", "Error saved user");
+            return "errors/error";
+        }
+        LOGGER.info("UserController: user was successfully registered");
+        session.setAttribute("user", savedUser.get());
         return "index";
     }
 
