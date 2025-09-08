9d897367adc1aeb198df5ceabce4b623881d8c99
aleks <darklissik.darklissik@gmail.com>
2025-09-09 02:16:20 +0300

    change JdbcSessionRepository.java

--- src/main/java/ru/aleks/weather/repository/JdbcSessionRepository.java
+++ src/main/java/ru/aleks/weather/repository/JdbcSessionRepository.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import ru.aleks.weather.mapper.SessionMapper;
 import ru.aleks.weather.model.Session;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public class JdbcSessionRepository implements SessionRepository {
 
     private final JdbcTemplate jdbcTemplate;
