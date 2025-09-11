3679dde88a08779c3f79ef805dd00ad2982b6fbf
aleks <darklissik.darklissik@gmail.com>
2025-09-12 00:55:26 +0300

    update UserController.java

--- src/main/java/ru/aleks/weather/controller/UserController.java
+++ src/main/java/ru/aleks/weather/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.aleks.weather.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -49,47 +50,127 @@ public class UserController {
         return "index";
     }
 
-    @GetMapping("/user/in")
+    @GetMapping("/login")
     public String getSingInPage(Model model) {
         model.addAttribute("user", new User());
         return "sign/sign-in";
     }
 
-    @PostMapping("/user/in")
-    public String signInUser(@ModelAttribute User user, Model model, HttpServletRequest request) {
+    @PostMapping("/login")
+    public String signInUser(@ModelAttribute User user, Model model, HttpServletRequest request, HttpServletResponse response) {
         Optional<User> foundUser = userService.getUserByLogin(user.getLogin());
         if (foundUser.isEmpty()) {
-            LOGGER.warn("UserController: User not found");
-            model.addAttribute("message", "User not found");
+            LOGGER.warn("UserController: user was not found");
+            model.addAttribute("message", "User was not found");
             return "errors/error";
         }
-        Cookie foundCookie = null;
-        for (Cookie cookie : request.getCookies()) {
-            if (cookie.getName().equals(NAMESESSION)) {
-                foundCookie = cookie;
-            }
+        Optional<Cookie> foundCookie = getCookieFromRequest(request);
+        if (foundCookie.isEmpty()) {
+            Session session = new Session(
+                    UUID.randomUUID(),
+                    userService.getUserByLogin(user.getLogin()).get().getId(),
+                    LocalDateTime.now()
+            );
+            sessionService.save(session);
+            Optional<Cookie> createdSessionCookie = createNewCookieForSession(session.getId());
+            Optional<Cookie> createdUserCookie = createNewCookieForUser(user.getLogin());
+            response.addCookie(createdUserCookie.get());
+            response.addCookie(createdSessionCookie.get());
+            request.getSession().setAttribute("username", user.getLogin());
+            return "/";
         }
-        UUID randomSessionId = UUID.randomUUID();
-        if (foundCookie == null) {
-            foundCookie = new Cookie(NAMESESSION, randomSessionId.toString());
-            foundCookie.setHttpOnly(true);
-            foundCookie.setPath("/");
-            foundCookie.setMaxAge(60 * 60);
-        } else {
-            UUID sessionId = UUID.fromString(foundCookie.getValue());
-            Optional<Session> foundSession = sessionService.getById(sessionId);
-            if (foundSession.isEmpty()) {
-                if ((Duration.between(LocalDateTime.now(), foundSession.get().getExpiresAt()).toMillis() > 3600)) {
-                    Session session = new Session();
-                    session.setId(randomSessionId);
-                    session.setUserId(user.getId());
-                    session.setExpiresAt(LocalDateTime.now());
-                    sessionService.save(session);
-                };
-            }
+        Optional<String> foundSessionId = getSessionIdFromCookie(foundCookie.get());
+        if (foundSessionId.isEmpty()) {
+            Session session = new Session(
+                    UUID.randomUUID(),
+                    userService.getUserByLogin(user.getLogin()).get().getId(),
+                    LocalDateTime.now()
+            );
+            sessionService.save(session);
+            Optional<Cookie> createdSessionCookie = createNewCookieForSession(session.getId());
+            Optional<Cookie> createdUserCookie = createNewCookieForUser(user.getLogin());
+            response.addCookie(createdUserCookie.get());
+            response.addCookie(createdSessionCookie.get());
+            request.getSession().setAttribute("username", user.getLogin());
+            return "/";
+        }
+        Optional<Session> foundSession = sessionService.getById(UUID.fromString(foundSessionId.get()));
+        if (foundSession.isEmpty()) {
+            Session session = new Session(
+                    UUID.randomUUID(),
+                    userService.getUserByLogin(user.getLogin()).get().getId(),
+                    LocalDateTime.now()
+            );
+            sessionService.save(session);
+            Optional<Cookie> createdSessionCookie = createNewCookieForSession(session.getId());
+            Optional<Cookie> createdUserCookie = createNewCookieForUser(user.getLogin());
+            response.addCookie(createdUserCookie.get());
+            response.addCookie(createdSessionCookie.get());
+            request.getSession().setAttribute("username", user.getLogin());
+            return "/";
+        }
+        LocalDateTime sessionTime = foundSession.get().getExpiresAt();
+        LocalDateTime now = LocalDateTime.now();
+        if (Duration.between(sessionTime, now).toMillis() > 3600) {
+            Session session = new Session(
+                    UUID.randomUUID(),
+                    userService.getUserByLogin(user.getLogin()).get().getId(),
+                    LocalDateTime.now()
+            );
+            sessionService.save(session);
+            Optional<Cookie> createdSessionCookie = createNewCookieForSession(session.getId());
+            Optional<Cookie> createdUserCookie = createNewCookieForUser(user.getLogin());
+            response.addCookie(createdUserCookie.get());
+            response.addCookie(createdSessionCookie.get());
             model.addAttribute("user", user.getLogin());
-            return "index";
+            return "redirect:/";
         }
-        return "index";
+        model.addAttribute("username", user.getLogin());
+        return "/";
+    }
+    private Optional<Cookie> createNewCookieForUser(String login) {
+        Cookie userCookie = new Cookie("username", userService.getUserByLogin(login).get().getLogin());
+        userCookie.setHttpOnly(true);
+        userCookie.setPath("/");
+        userCookie.setMaxAge(60 * 60);
+        return Optional.of(userCookie);
+    }
+
+    private Optional<Cookie> createNewCookieForSession(UUID sessionId) {
+        Optional<Cookie> createdCookie = Optional.empty();
+        try {
+            Cookie cookie = new Cookie(NAMESESSION, String.valueOf(sessionId));
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+            cookie.setMaxAge(60 * 60);
+            createdCookie = Optional.of(cookie);
+        } catch (Exception exception) {
+            LOGGER.warn("UserController: error create new cookie");
+        }
+        return createdCookie;
+    }
+
+    private Optional<String> getSessionIdFromCookie(Cookie cookie) {
+        Optional<String> foundSessionId = Optional.empty();
+        if (cookie.getName().equals(NAMESESSION)) {
+            foundSessionId = Optional.ofNullable(
+                    cookie.getValue()
+            );
+        }
+        return foundSessionId;
+    }
+
+    private Optional<Cookie> getCookieFromRequest(HttpServletRequest request) {
+        Optional<Cookie> foundCookie = Optional.empty();
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals(NAMESESSION)) {
+                    foundCookie = Optional.of(
+                            cookie
+                    );
+                }
+            }
+        }
+        return foundCookie;
     }
 }
