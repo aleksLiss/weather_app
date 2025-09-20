0a3f704f5c4ed7ae4107d7746fbe1756c43817ad
aleks <darklissik.darklissik@gmail.com>
2025-09-20 11:28:46 +0300

    add storage CommonUsernames.java

---
+++ src/main/java/ru/aleks/weather/check/CommonUsernames.java
@@ -0,0 +1,20 @@
+package ru.aleks.weather.check;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CommonUsernames {
+
+    public static final List<String> COMMON_USERNAMES = List.of(
+            "gay",
+            "sex",
+            "gaysex"
+    );
+
+    public boolean isContains(String username) {
+        return COMMON_USERNAMES.contains(username.toLowerCase());
+    }
+
+}
