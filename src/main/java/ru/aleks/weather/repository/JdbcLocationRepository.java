6383b72eec57c6ef8b0512f3b4c01964e59430d0
aleksLiss <darklissik.darklissik@gmail.com>
2025-10-01 11:38:21 +0300

    update JdbcLocationRepository.java

--- src/main/java/ru/aleks/weather/repository/JdbcLocationRepository.java
+++ src/main/java/ru/aleks/weather/repository/JdbcLocationRepository.java
@@ -2,10 +2,13 @@ package ru.aleks.weather.repository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.aleks.weather.mapper.LocationMapper;
 import ru.aleks.weather.model.Location;
+import ru.aleks.weather.model.User;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,38 +29,40 @@ public class JdbcLocationRepository implements LocationRepository {
     @Override
     public Optional<Location> save(Location location) {
         String sql = "INSERT INTO locations(name, user_id, latitude, longitude) VALUES(?, ?, ?, ?)";
-        Optional<Location> savedLocation;
+        Optional<Location> savedLocation = Optional.empty();
         try {
-            jdbcTemplate.update(sql,
+            jdbcTemplate.update(
+                    sql,
                     location.getName(),
                     location.getUserId(),
                     location.getLatitude(),
-                    location.getLongitude());
-            savedLocation = Optional.ofNullable(
-                    jdbcTemplate.queryForObject("SELECT * FROM locations WHERE name = ?",
-                            new Object[]{location.getName()},
-                            locationMapper)
+                    location.getLongitude()
             );
-            LOGGER.info("LocationRepository: location was saved successfully");
-        } catch (Exception ex) {
-            LOGGER.warn("LocationRepository: location was not saved");
-            throw new RuntimeException("error save location");
+            savedLocation = Optional.ofNullable(
+                    jdbcTemplate
+                            .queryForObject(
+                                    "SELECT * FROM locations WHERE NAME = ?", new Object[]{location.getName()}, locationMapper
+                            ));
+            LOGGER.info("LocationRepository: Location was saved");
+        } catch (DuplicateKeyException ex) {
+            LOGGER.warn("LocationRepository: Location was not saved");
+            throw new RuntimeException("location was not saved");
         }
         return savedLocation;
     }
 
     @Override
     public Optional<Location> getByName(String name) {
-        String sql = "SELECT * FROM locations WHERE name = ?";
-        Optional<Location> foundLocation;
+        String sql = "SELECT * FROM locations WHERE NAME = ?";
+        Optional<Location> foundLocation = Optional.empty();
         try {
             foundLocation = Optional.ofNullable(
-                    jdbcTemplate.queryForObject(
-                            sql,
-                            new Object[]{name},
-                            locationMapper
-                    )
-            );
+                    jdbcTemplate
+                            .queryForObject(
+                                    sql,
+                                    new Object[]{name},
+                                    locationMapper
+                            ));
             LOGGER.info("LocationRepository: location by name was found");
         } catch (Exception ex) {
             LOGGER.warn("LocationRepository: location by name was not found");
@@ -91,9 +96,9 @@ public class JdbcLocationRepository implements LocationRepository {
         String sql = "SELECT * FROM locations WHERE user_id = ?";
         List<Location> foundLocationsByUserId;
         try {
-            foundLocationsByUserId = jdbcTemplate.queryForList(
+            foundLocationsByUserId = jdbcTemplate.query(
                     sql,
-                    Location.class,
+                    new BeanPropertyRowMapper<>(Location.class),
                     userId
             );
             LOGGER.info("LocationRepository: locations by user id was found");
@@ -116,19 +121,5 @@ public class JdbcLocationRepository implements LocationRepository {
             throw new RuntimeException("error delete location by user id");
         }
         return deleted != 0;
-    }
-
-    @Override
-    public boolean updateByUserId(int userId, double newLatitude, double newLongitude) {
-        int updated;
-        String sql = "UPDATE locations SET latitude = ?, longitude = ? WHERE user_id = ?";
-        try {
-            updated = jdbcTemplate.update(sql, newLatitude, newLongitude, userId);
-            LOGGER.info("LocationRepository: location by user id was updated");
-        } catch (Exception ex) {
-            LOGGER.warn("LocationRepository: location by user id was not updated");
-            throw new RuntimeException("error update location by user id");
-        }
-        return updated != 0;
     }
 }
