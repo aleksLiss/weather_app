166c76f15b4e5e7c741ee74411a3cc4fc9b1e689
aleksLiss <darklissik.darklissik@gmail.com>
2025-10-06 22:40:27 +0300

    updated LocationController.java

--- src/main/java/ru/aleks/weather/controller/LocationController.java
+++ src/main/java/ru/aleks/weather/controller/LocationController.java
@@ -1,18 +1,17 @@
 package ru.aleks.weather.controller;
 
-import org.springframework.expression.spel.ast.OpAnd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.aleks.weather.dto.LocationAnswerDto;
-import ru.aleks.weather.dto.LocationCityNameDto;
-import ru.aleks.weather.dto.LocationCoordinatesDto;
-import ru.aleks.weather.model.Location;
+import ru.aleks.weather.dto.LocationGetDto;
 import ru.aleks.weather.service.LocationService;
 import ru.aleks.weather.service.UserService;
 import ru.aleks.weather.service.WeatherApiService;
+import ru.aleks.weather.utils.CheckCoordinates;
+import ru.aleks.weather.utils.TemperatureTransformer;
 
 import java.util.Optional;
 
@@ -22,42 +21,69 @@ public class LocationController {
     private final LocationService locationService;
     private final UserService userService;
     private final WeatherApiService weatherApiService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocationController.class);
+    private final TemperatureTransformer temperatureTransformer;
+    private final CheckCoordinates checkCoordinates;
 
-    public LocationController(LocationService locationService, UserService userService, WeatherApiService weatherApiService) {
+    public LocationController(LocationService locationService, UserService userService, WeatherApiService weatherApiService, TemperatureTransformer temperatureTransformer, CheckCoordinates checkCoordinates) {
         this.locationService = locationService;
         this.userService = userService;
         this.weatherApiService = weatherApiService;
+        this.temperatureTransformer = temperatureTransformer;
+        this.checkCoordinates = checkCoordinates;
     }
 
     @GetMapping("/location/city")
-    public String getSearchLocationPage(@ModelAttribute LocationAnswerDto locationAnswerDto, Model model) {
+    public String getSearchLocationPage(Model model) {
         model.addAttribute("location", new LocationAnswerDto());
         return "search-results";
     }
 
     @PostMapping("/location/city")
-    public String getLocationByCityName(@ModelAttribute LocationCityNameDto locationCityNameDto,
-                                                   Model model) {
-        System.out.println(locationCityNameDto.getCity());
-        try {
-            Optional<LocationAnswerDto> answerDto = weatherApiService.getWeatherByName(locationCityNameDto.getCity());
-            System.out.println(answerDto.get().getCity());
-            model.addAttribute("city", "minsk");
-            model.addAttribute("temperature", "25");
-            model.addAttribute("humidity", "70");
-            model.addAttribute("country", "BY");
-            model.addAttribute("description", "descrip");
-        } catch (Exception ex) {
-            model.addAttribute("message", "city not found");
-            return "errors/404";
+    public String getLocationFromApi(@ModelAttribute LocationGetDto locationGetDto,
+                                        Model model) {
+
+        String locationFromDto = locationGetDto.getLocation();
+        if (!checkCoordinates.checkCoordinates(locationFromDto)) {
+            LOGGER.warn("LocationController: city or coordinates not correct");
+            model.addAttribute("message", "city or coordinates not correct");
+            return "errors/error";
+        }
+        if (locationGetDto.getLocation() != null) {
+            try {
+            Optional<LocationAnswerDto> answerDto = weatherApiService.getWeatherByName(locationGetDto.getLocation());
+                if (answerDto.isPresent()) {
+                    LocationAnswerDto locationAnswerDto = answerDto.get();
+                    model.addAttribute("cc", locationAnswerDto.getCity());
+                    model.addAttribute("temperature", String.format("%.1f", temperatureTransformer
+                            .fromKelvinsToCelsius(
+                                    locationAnswerDto.getTemperature())));
+                    model.addAttribute("humidity", locationAnswerDto.getHumidity());
+                    model.addAttribute("country", locationAnswerDto.getCountry());
+                    model.addAttribute("description", locationAnswerDto.getDescription());
+                }
+            } catch (Exception ex) {
+                model.addAttribute("message", "city not found");
+                return "errors/error";
+            }
+        } else {
+            try {
+                Optional<LocationAnswerDto> answerDto = weatherApiService.getWeatherByLocation(52.427063, 30.987352);
+                if (answerDto.isPresent()) {
+                    LocationAnswerDto locationAnswerDto = answerDto.get();
+                    model.addAttribute("cc", locationAnswerDto.getCity());
+                    model.addAttribute("temperature", String.format("%.1f", temperatureTransformer
+                            .fromKelvinsToCelsius(
+                                    locationAnswerDto.getTemperature())));
+                    model.addAttribute("humidity", locationAnswerDto.getHumidity());
+                    model.addAttribute("country", locationAnswerDto.getCountry());
+                    model.addAttribute("description", locationAnswerDto.getDescription());
+                }
+            } catch (Exception ex) {
+                model.addAttribute("message", "city not found");
+                return "errors/error";
+            }
         }
         return "search-results";
-    }
-
-    @PostMapping("/coordinates")
-    public String getLocationByCoordinates(@ModelAttribute LocationCoordinatesDto locationCoordinatesDto,
-                                                      Model model) {
-        // check: latitude = from -90 to 90, longitude = from -180 to 180
-        return "";
     }
 }
