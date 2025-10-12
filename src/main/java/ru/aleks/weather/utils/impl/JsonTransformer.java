9cdddd054429423897e4a12b050521923cf2ee9e
aleksLiss <darklissik.darklissik@gmail.com>
2025-10-12 17:47:52 +0300

    updated JsonTransformer.java

--- src/main/java/ru/aleks/weather/utils/impl/JsonTransformer.java
+++ src/main/java/ru/aleks/weather/utils/impl/JsonTransformer.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import ru.aleks.weather.dto.LocationAnswerDto;
+import ru.aleks.weather.dto.LocationTransform;
 import ru.aleks.weather.utils.FormatTransformer;
 
 import java.util.Optional;
@@ -21,11 +21,11 @@ public class JsonTransformer implements FormatTransformer {
     }
 
     @Override
-    public Optional<LocationAnswerDto> transform(String json) {
-        LocationAnswerDto locationAnswerDto = new LocationAnswerDto();
+    public Optional<LocationTransform> transform(String json) {
+        LocationTransform locationAnswerDto = new LocationTransform();
         object.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         try {
-            locationAnswerDto = object.readValue(json, LocationAnswerDto.class);
+            locationAnswerDto = object.readValue(json, LocationTransform.class);
         } catch (Exception ex) {
             LOGGER.warn("JsonTransformer: error transform from json to LocationAnswerDto");
         }
