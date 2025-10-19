cee230abc0e700ff0fc95fb32ae8a6ef4513cd04
aleksLiss <darklissik.darklissik@gmail.com>
2025-10-19 13:21:12 +0300

    updated TokenConfig.java

--- src/main/java/ru/aleks/weather/config/TokenConfig.java
+++ src/main/java/ru/aleks/weather/config/TokenConfig.java
@@ -1,9 +1,7 @@
 package ru.aleks.weather.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
