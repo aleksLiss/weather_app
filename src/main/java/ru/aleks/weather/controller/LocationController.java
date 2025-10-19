345b76b94885f62bf1a6b6d48430a4c5a2abcb9e
aleksLiss <darklissik.darklissik@gmail.com>
2025-10-19 21:19:35 +0300

    updated LocationController

--- src/main/java/ru/aleks/weather/controller/LocationController.java
+++ src/main/java/ru/aleks/weather/controller/LocationController.java
@@ -1,26 +1,22 @@
 package ru.aleks.weather.controller;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.aleks.weather.dto.FoundWeatherDto;
-import ru.aleks.weather.dto.LocationTransform;
-import ru.aleks.weather.dto.LocationGetDto;
+import ru.aleks.weather.auth.AuthenticateUser;
+import ru.aleks.weather.dto.*;
+import ru.aleks.weather.exception.*;
 import ru.aleks.weather.model.Location;
 import ru.aleks.weather.model.User;
 import ru.aleks.weather.service.LocationService;
-import ru.aleks.weather.service.UserService;
 import ru.aleks.weather.service.WeatherApiService;
 import ru.aleks.weather.utils.CheckLocations;
-import ru.aleks.weather.utils.TemperatureTransformer;
+import ru.aleks.weather.utils.GetCoordinatesFromString;
 
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static ru.aleks.weather.utils.Locations.CITY;
 import static ru.aleks.weather.utils.Locations.COORDINATES;
@@ -29,18 +25,22 @@ import static ru.aleks.weather.utils.Locations.COORDINATES;
 public class LocationController {
 
     private final LocationService locationService;
-    private final UserService userService;
     private final WeatherApiService weatherApiService;
     private static final Logger LOGGER = LoggerFactory.getLogger(LocationController.class);
-    private final TemperatureTransformer temperatureTransformer;
     private final CheckLocations checkCoordinates;
+    private final AuthenticateUser authenticateUser;
+    private final DtoSetter<LocationSendDto> locationSendDtoDtoSetter;
+    private final GetCoordinatesFromString getCoordinatesFromString;
+    private final ModelSetter<Location> locationModelSetter;
 
-    public LocationController(LocationService locationService, UserService userService, WeatherApiService weatherApiService, TemperatureTransformer temperatureTransformer, CheckLocations checkCoordinates) {
+    public LocationController(LocationService locationService, WeatherApiService weatherApiService, CheckLocations checkCoordinates, AuthenticateUser authenticateUser, DtoSetter<LocationSendDto> locationSendDtoDtoSetter, GetCoordinatesFromString getCoordinatesFromString, ModelSetter<Location> locationModelSetter) {
         this.locationService = locationService;
-        this.userService = userService;
         this.weatherApiService = weatherApiService;
-        this.temperatureTransformer = temperatureTransformer;
         this.checkCoordinates = checkCoordinates;
+        this.authenticateUser = authenticateUser;
+        this.locationSendDtoDtoSetter = locationSendDtoDtoSetter;
+        this.getCoordinatesFromString = getCoordinatesFromString;
+        this.locationModelSetter = locationModelSetter;
     }
 
     @GetMapping("/location/city")
@@ -52,116 +52,67 @@ public class LocationController {
     public String getLocationFromApi(@ModelAttribute LocationGetDto locationGetDto,
                                      Model model,
                                      HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        String foundUserName = null;
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("username")) {
-                LOGGER.info("IndexController: cookie value with name 'username': " + cookie.getValue());
-                foundUserName = cookie.getValue();
-            }
-        }
-        if (foundUserName != null) {
-            model.addAttribute("username", foundUserName);
+        try {
+            authenticateUser.findUser(request, model);
+        } catch (UserNotFoundException ex) {
+            return createExceptionMessage(model, ex.getMessage());
+        } catch (Exception ex) {
+            return createExceptionMessage(model, ex.getMessage());
         }
         String locationFromDto = locationGetDto.getLocation();
         try {
             if (checkCoordinates.checkLocationByRegex(CITY, locationFromDto)) {
-                Optional<LocationTransform> answerDto = weatherApiService.getWeatherByCityName(locationFromDto);
-                FoundWeatherDto foundWeatherDto = getFoundWeatherDto(answerDto.get());
-                String urlForImg = "https://openweathermap.org/img/wn/" + answerDto.get().getIcon() + ".png";
-                LOGGER.warn("icon location found : " + urlForImg);
-                foundWeatherDto.setIcon(urlForImg);
-                model.addAttribute("weather", foundWeatherDto);
+                Optional<LocationTransform> locationTransform = weatherApiService.getWeatherByCityName(locationFromDto);
+                LocationSendDto locationSendDto = locationSendDtoDtoSetter.setDto(locationTransform.get());
+                model.addAttribute("locationSendDto", locationSendDto);
                 return "search-results";
             }
-        } catch (Exception ex) {
-            LOGGER.warn("LocationController: name city incorrect");
-            model.addAttribute("message", "incorrect city name");
-            return "errors/error";
-        }
-        try {
             if (checkCoordinates.checkLocationByRegex(COORDINATES, locationFromDto)) {
-                Pattern patt = Pattern.compile("^\\s*(\\d+(?:\\.\\d+)?)\\s*[ ,]\\s*(\\d+(?:\\.\\d+)?)\\s*$");
-                Matcher matcher = patt.matcher(locationFromDto);
-                if (!matcher.matches()) {
-                    LOGGER.warn("LocationController: incorrect coordinates");
-                    model.addAttribute("message", "incorrect coordinates");
-                    return "errors/error";
-                }
-                double latitude = Double.parseDouble(matcher.group(1));
-                double longitude = Double.parseDouble(matcher.group(2));
+                double latitude = getCoordinatesFromString.getCoordinates(locationFromDto)[0];
+                double longitude = getCoordinatesFromString.getCoordinates(locationFromDto)[1];
                 Optional<LocationTransform> locationTransform = weatherApiService.getWeatherByCoordinates(latitude, longitude);
-                FoundWeatherDto foundWeatherDto = getFoundWeatherDto(locationTransform.get());
-                String urlForImg = "https://openweathermap.org/img/wn/" + locationTransform.get().getIcon() + ".png";
-                LOGGER.warn("icon location found : " + urlForImg);
-                foundWeatherDto.setIcon(urlForImg);
-                model.addAttribute("weather", foundWeatherDto);
+                LocationSendDto locationSendDto = locationSendDtoDtoSetter.setDto(locationTransform.get());
+                model.addAttribute("locationSendDto", locationSendDto);
                 return "search-results";
             }
+        } catch (LocationNotFoundException | FailedTransformJsonToDto ex) {
+            return createExceptionMessage(model, ex.getMessage());
         } catch (Exception ex) {
-            LOGGER.warn("LocationController: coordinates incorrect");
-            model.addAttribute("message", "incorrect coordinates");
-            return "errors/error";
+            return createExceptionMessage(model, ex.getMessage());
         }
-        model.addAttribute("weather", new FoundWeatherDto());
-        LOGGER.warn("LocationController: city or coordinates not correct");
-        model.addAttribute("message", "city or coordinates not correct");
-        return "/errors/error";
+        return createExceptionMessage(model, "exception unknown");
     }
 
     @PostMapping("/location/add")
-    public String addLocationToUser(@ModelAttribute("weather") FoundWeatherDto foundWeatherDto,
+    public String addLocationToUser(@ModelAttribute LocationSendDto locationSendDto,
                                     HttpServletRequest request,
                                     Model model) {
-        Cookie[] cookies = request.getCookies();
-        String username = "";
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("username")) {
-                username = cookie.getValue();
-                break;
-            }
+        Optional<User> foundUser;
+        int userId;
+        try {
+            foundUser = authenticateUser.findUser(request, model);
+            userId = foundUser.get().getId();
+        } catch (UserNotFoundException ex) {
+            return createExceptionMessage(model, ex.getMessage());
         }
-        Optional<User> foundUser = userService.getUserByLogin(username);
-        if (foundUser.isEmpty()) {
-            model.addAttribute("username", "Гость");
+        String locationFromDto = locationSendDto.getCityName();
+        try {
+            Optional<LocationTransform> locationTransform = weatherApiService.getWeatherByCityName(locationFromDto);
+            LOGGER.warn("city name from trans   " + locationTransform.get().getCity());
+            Location location = locationModelSetter.modelSet(locationTransform.get(), userId);
+            locationService.save(location);
+            return "redirect:/";
+        } catch (LocationNotFoundException | FailedTransformJsonToDto ex) {
+            return createExceptionMessage(model, ex.getMessage());
+        } catch (Exception ex) {
+            return createExceptionMessage(model, ex.getMessage());
         }
-        model.addAttribute("username", foundUser.get());
-        int id = foundUser.get().getId();
-        Location location = new Location();
-
-        Optional<LocationTransform> locationTransform = weatherApiService.getWeatherByCityName(foundWeatherDto.getName());
-
-        location.setName(foundWeatherDto.getName());
-        location.setUserId(id);
-
-        location.setLatitude(locationTransform.get().getCoordsDto().getLat());
-        location.setLongitude(locationTransform.get().getCoordsDto().getLon());
-
-        locationService.save(location);
-
-        return "redirect:/";
     }
 
-    private FoundWeatherDto getFoundWeatherDto(LocationTransform answerDto) {
-        FoundWeatherDto foundWeatherDto = new FoundWeatherDto();
-        foundWeatherDto.setCountry(
-                answerDto.getCountry()
-        );
-        foundWeatherDto.setHumidity(
-                answerDto.getHumidity()
-        );
-        foundWeatherDto.setDescription(
-                answerDto.getDescription()
-        );
-        foundWeatherDto.setName(
-                answerDto.getCity()
-        );
-        foundWeatherDto.setTemperature(
-                String.format("%.1f",
-                        temperatureTransformer.fromKelvinsToCelsius(
-                                answerDto.getTemperature()
-                        ))
-        );
-        return foundWeatherDto;
+    private String createExceptionMessage(Model model, String msg) {
+        ExceptionDto exceptionDto = new ExceptionDto(msg);
+        LOGGER.warn("IndexController: {}", exceptionDto.getMessage());
+        model.addAttribute("exceptionDto", exceptionDto);
+        return "errors/error";
     }
 }
