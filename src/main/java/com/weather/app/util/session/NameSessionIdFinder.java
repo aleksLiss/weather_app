2059d3d0a57e10c59ae19135b005c14d2ada0849
aleksLiss <darklissik.darklissik@gmail.com>
2025-12-23 22:15:46 +0300

    add NameSessionIdFinder

---
+++ src/main/java/com/weather/app/util/session/NameSessionIdFinder.java
@@ -0,0 +1,17 @@
+package com.weather.app.util.session;
+
+import com.weather.app.exception.session.NameSessionIdNotFound;
+import org.springframework.core.env.Environment;
+
+public class NameSessionIdFinder {
+
+    private NameSessionIdFinder(){}
+
+    public static String getNameSessionId(Environment environment) {
+        String nameSessionId = environment.getProperty("spring.application.session.id.name");
+        if (null == nameSessionId) {
+            throw new NameSessionIdNotFound("Name session id was not found");
+        }
+        return nameSessionId;
+    }
+}
