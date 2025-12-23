2478cafadd5f350bad1c8b81ec1b305a25d182e8
aleksLiss <darklissik.darklissik@gmail.com>
2025-12-23 22:04:08 +0300

    add MainData

---
+++ src/main/java/com/weather/app/dto/MainData.java
@@ -0,0 +1,28 @@
+package com.weather.app.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MainData {
+
+    @JsonProperty("temp")
+    private double temperature;
+    private int humidity;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+}
