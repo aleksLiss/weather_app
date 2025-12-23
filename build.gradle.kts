04dcd32565c4092d319599ec73f02debd326f0c5
aleksLiss <darklissik.darklissik@gmail.com>
2025-12-23 22:40:41 +0300

    add gradle files and Main.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id ("jacoco")
 }
 
 group = "com.weather.app"
@@ -9,10 +10,24 @@ repositories {
     mavenCentral()
 }
 
+jacoco {
+    toolVersion = "0.8.12"
+}
+
 dependencies {
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.8")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.5.7")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.5.6")
+    implementation("org.liquibase:liquibase-core:5.0.0")
+    implementation("org.postgresql:postgresql:42.7.7")
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.8")
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")
+    testImplementation("com.h2database:h2:2.3.232")
 }
 
 tasks.test {
